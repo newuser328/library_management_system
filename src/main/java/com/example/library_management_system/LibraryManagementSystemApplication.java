b4df6f17fef4773455901789213192c2c566801e
newuser328 <newuser328>
2026-01-13 16:11:16 +0800

    已修复

--- src/main/java/com/example/library_management_system/LibraryManagementSystemApplication.java
+++ src/main/java/com/example/library_management_system/LibraryManagementSystemApplication.java
@@ -2,8 +2,9 @@ package com.example.library_management_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.library_management_system")
 public class LibraryManagementSystemApplication {
 
     public static void main(String[] args) {
